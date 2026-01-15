8137ec70cae5d3070fa41aaa5ec8fee605b4ea40
5533Robotics <132020260+5533Robotics@users.noreply.github.com>
2026-01-15 18:34:26 -0500

    kitbot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.CANFuelSubsystem;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -42,6 +43,7 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    public final CANFuelSubsystem fuelSubsystem = new CANFuelSubsystem();
 
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
@@ -106,6 +108,10 @@ public class RobotContainer {
 
         // Reset the field-centric heading on left bumper press.
         joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+        // Fuel subsystem controls
+        joystick.rightTrigger().whileTrue(fuelSubsystem.run(() -> fuelSubsystem.intake()).finallyDo(() -> fuelSubsystem.stop()));
+        joystick.leftTrigger().whileTrue(fuelSubsystem.run(() -> fuelSubsystem.launch()).finallyDo(() -> fuelSubsystem.stop()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
