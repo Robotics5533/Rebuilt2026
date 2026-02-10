2938f931a35659eaa36c0313d0c3b870227d3e7b
cinnamonsroll <cinnamonsrowolls@gmail.com>
2026-02-09 20:54:00 -0500

    Get rid of useless comments

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -36,16 +36,12 @@ public class LimelightSubsystem extends SubsystemBase {
     posePublisher = telemetryTable
         .getStructTopic("Estimated Robot Pose", Pose2d.struct)
         .publish();
-
-    // Initialize the dashboard toggle
     if (!SmartDashboard.containsKey("Vision/Enabled")) {
       SmartDashboard.putBoolean("Vision/Enabled", Constants.LimelightConstants.ENABLE_VISION_ODOMETRY);
     }
   }   
 
   public Optional<Measurement> getMeasurement(Pose2d currentRobotPose) {
-
-    // 1. Check if Vision is Enabled via Dashboard
     if (!SmartDashboard.getBoolean("Vision/Enabled", true)) {
       return Optional.empty();
     }
@@ -66,12 +62,9 @@ public class LimelightSubsystem extends SubsystemBase {
 
     Pose2d visionPose = poseEstimate.pose;
 
-    // 2. Distance Rejection: If the vision pose is too far from odometry, reject it (unless we are lost/initializing)
-    // We only apply this check if we have a valid current pose (not at 0,0)
     if (currentRobotPose != null && currentRobotPose.getTranslation().getNorm() > 0.1) {
        double distance = currentRobotPose.getTranslation().getDistance(visionPose.getTranslation());
        if (distance > Constants.LimelightConstants.VISION_REJECTION_DISTANCE_THRESHOLD_METERS) {
-           // Optional: Log rejection
            return Optional.empty();
        }
     }
@@ -83,7 +76,6 @@ public class LimelightSubsystem extends SubsystemBase {
     double xyStDev = 0.25 * poseEstimate.avgTagDist;
     double degStDev = 8.0;
     
-    // 3. Speed Rejection/Scaling: Trust vision less when moving fast
     if (speed > Constants.LimelightConstants.VISION_REJECTION_SPEED_THRESHOLD_MPS) {
       xyStDev *= 1.5;
       degStDev *= 1.5;
